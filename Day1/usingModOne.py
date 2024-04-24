9b30ac367bed2268a2712f212c02b8e7c1d307a9
Kaushik Sonowal <106921631+KaushikSonowal@users.noreply.github.com>
2024-04-24 17:07:53 +0530

    Add files via upload

---
+++ Day1/usingModOne.py
@@ -0,0 +1,8 @@
+import moduleIntro
+
+moduleIntro.funOne()
+moduleIntro.funTwo()
+print(moduleIntro.dataOne)
+print(moduleIntro.dataTwo)
+
+
---
+++ Day1/usingModThree.py
@@ -0,0 +1,9 @@
+'''
+   Importing all symbols (functions/classes/data) directly
+
+'''
+
+from moduleIntro import funOne, dataOne
+
+funOne()
+print(dataOne)
